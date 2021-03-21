a430c85555840a700b13563206284b2139a69f9f
abaransy <baransymusic@gmail.com>
2021-03-20 17:49:36 -0700

    much more concise and simple LFU implementation

--- problems/lfu.py
+++ problems/lfu.py
@@ -41,87 +41,41 @@
 #                  // cache=[3,4], cnt(4)=2, cnt(3)=3
 import collections
 
-class Node:
-    def __init__(self, key, val):
-        self.key = key
-        self.val = val
-        self.freq = 1
-        self.prev = self.next = None
-
-class DLinkedList:
-    def __init__(self):
-        self._sentinel = Node(None, None) # dummy node
-        self._sentinel.next = self._sentinel.prev = self._sentinel
-        self._size = 0
-
-    def __len__(self):
-        return self._size
-
-    def append(self, node):
-        node.next = self._sentinel.next
-        node.prev = self._sentinel
-        node.next.prev = node
-        self._sentinel.next = node
-        self._size += 1
-
-    def pop(self, node=None):
-        if self._size == 0:
-            return
-
-        if not node:
-            node = self._sentinel.prev
-
-        node.prev.next = node.next
-        node.next.prev = node.prev
-        self._size -= 1
-
-        return node
-
 class LFUCache:
-    def __init__(self, capacity):
-        self._size = 0
-        self._capacity = capacity
-        self._node = dict() # key: Node
-        self._freq = collections.defaultdict(DLinkedList)
-        self._minfreq = 0
 
-    def _update(self, node):
-        freq = node.freq
+    def __init__(self, capacity: int):
+        self.d = {} # maps key to frequency
+        self.f = defaultdict(dict) # first key is the frequency, second key is the key of the key value pair
+        self.c = capacity
+        self.m = 1 # minimum frequency
 
-        self._freq[freq].pop(node)
-
-        if self._minfreq == freq and not self._freq[freq]:
-            self._minfreq += 1
-
-        node.freq += 1
-        freq = node.freq
-        self._freq[freq].append(node)
-
-    def get(self, key):
-        if key not in self._node:
+    def get(self, key: int) -> int:
+        if key not in self.d:
             return -1
 
-        node = self._node[key]
-        self._update(node)
-        return node.val
+        x = self.d[key]
+        v = self.f[x].pop(key)
 
-    def put(self, key, value):
-        if self._capacity == 0:
-            return
+        if not self.f[x] and self.m == x: # if there are no more elements that have the current minimum frequency
+            self.m += 1
 
-        if key in self._node:
-            node = self._node[key]
-            node.val = value
+        self.f[x + 1][key] = v
+        self.d[key] = x + 1
 
-            self._update(node)
-        else:
-            if self._size == self._capacity:
-                node = self._freq[self._minfreq].pop()
-                del self._node[node.key]
-                self._size -= 1
+        return v
 
-            node = Node(key, value)
-            self._node[key] = node
-            self._freq[1].append(node)
-            self._minfreq = 1
-            self._size += 1
+    def put(self, key: int, value: int) -> None:
+        if self.c > 0:
+            if key in self.d: # if the key has been accessed before
+                self.get(key)
+                self.f[self.d[key]][key] = value
+            else:
+                if len(self.d) == self.c:
+                    k = next(iter(self.f[self.m])) # gets first item in the dictionary, looks like dictionaries are order in python since version 3.6
+
+                    self.f[self.m].pop(k)
+                    self.d.pop(k)
+
+                self.d[key] = 1
+                self.f[1][key] = value
+                self.m = 1
